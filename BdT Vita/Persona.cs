6552b4f265919e79e39cd8fcf22df75db270b98b
konkas7 <76922391+konkas7@users.noreply.github.com>
2023-09-08 14:13:08 +0200

    inizializzazione classi

---
+++ BdT Vita/Persona.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BdT_Vita
+{
+    public class Persona : IEquatable<Persona>
+    {
+        public string _nome;
+        public string _cognome;
+        public double _telefono;
+        public int _debito;
+        public bool _segreteria;
+
+        
+        
+        [JsonProperty]
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            private set
+            {
+                if (!String.IsNullOrWhiteSpace(value))
+                    _nome = value;
+                else
+                    throw new Exception("Nome non valido");
+            }
+        }
+        [JsonProperty]
+
+        public string Cognome
+        {
+            get
+            {
+                return _cognome;
+            }
+            private set
+            {
+                if (!String.IsNullOrWhiteSpace(value))
+                    _cognome = value;
+                else
+                    throw new Exception("Cognome non valido");
+            }
+        }
+        [JsonProperty]
+        public double Telefono
+        {
+            get
+            {
+                return _telefono;
+            }
+            private set
+            {
+                if (!String.IsNullOrWhiteSpace(value.ToString()) && value.ToString().Length == 10)
+                    _telefono = value;
+                else
+                    throw new Exception("Numero di Telefono non valido");
+            }
+        }
+        [JsonProperty]
+        public int Debito
+        {
+            get
+            {
+                return _debito;
+            }
+            set
+            {
+                _debito = value;
+            }
+        }
+        [JsonProperty]
+        public bool Segreteria
+        {
+            get
+            {
+                return _segreteria;
+            }
+            private set
+            {
+                if (!String.IsNullOrWhiteSpace(value.ToString()))
+                    _segreteria = value;
+                else
+                    throw new Exception("Partecipazione alla segreteria non valida");
+            }
+        }
+
+        [JsonProperty]
+        public List<Prestazione> Prestazioni { get; set; } // Elenco delle prestazioni offerte o ricevute dal socio
+
+        public Socio(string cognome, string nome, double telefono, int debito, bool segreteria)
+        {
+            Cognome = cognome;
+            Nome = nome;
+            Telefono = telefono;
+            Debito = debito;
+            Segreteria = segreteria;
+            Prestazioni = new List<Prestazione>();
+        }
+        public Socio()
+        {
+            Cognome = "NoData";
+            Nome = "NoData";
+            Telefono = 1000000000;
+            Debito = 0;
+            Segreteria = false;
+            Prestazioni = new List<Prestazione>();
+        }
+        // Metodo per aggiungere una prestazione alla lista del socio
+        public void AggiungiPrestazione(Prestazione prestazione)
+        {
+            Prestazioni.Add(prestazione);
+        }
+
+        // Metodo per calcolare il saldo del socio (ore erogate - ore ricevute)
+        public int CalcolaDebito()
+        {
+            int oreErogate = 0;
+            int oreRicevute = 0;
+
+            foreach (Prestazione prestazione in Prestazioni)
+            {
+                if (prestazione.Erogatore.Equals(this))
+                {
+                    oreErogate += prestazione.Ore;
+                }
+                else if (prestazione.Ricevente.Equals(this))
+                {
+                    oreRicevute += prestazione.Ore;
+                }
+            }
+
+            this.Debito = oreRicevute - oreErogate;
+            return this.Debito;
+        }
+
+        protected Socio(Socio other) : this(other.Cognome, other.Nome, other.Telefono, other.Debito, other.Segreteria)
+        {
+
+        }
+        public Socio Clone()
+        {
+            return new Socio(this);
+        }
+        public bool Equals(Socio u)
+        {
+            if (u == null) return false;
+
+            if (this == u) return true;
+
+            return (this.Cognome == u.Cognome && this.Nome == u.Nome);
+        }
+
+        public override string ToString()
+        {
+            return $"Socio: {Cognome} {Nome}; {Telefono}, {Debito}";
+        }
+    }
+}
---
+++ BdT Vita/Prestazione.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BdT_Vita
+{
+    public class Prestazione : IEquatable<Prestazione>
+    {
+        public string _id;
+        public Persona _giver; 
+        public Persona _reciver; 
+        public int _ore;    
+        public DateTime _data; 
+        public string _tipo; 
+
+        [JsonProperty]
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+            private set
+            {
+                if (!String.IsNullOrWhiteSpace(value))
+                    _id = value;
+                else
+                    throw new Exception("Id non valido");
+            }
+        }
+        [JsonProperty]
+        public Persona Giver
+        {
+            get
+            {
+                return _giver;
+            }
+            private set
+            {
+                if (value != null)
+                    _giver = value;
+                else
+                    throw new Exception("Erogatore non valido");
+            }
+        }
+        [JsonProperty]
+        public Persona Reciver
+        {
+            get
+            {
+                return _reciver;
+            }
+            private set
+            {
+                if (value != null)
+                    _reciver = value;
+                else
+                    throw new Exception("Ricevente non valido");
+            }
+        }
+        [JsonProperty]
+        public int Ore
+        {
+            get
+            {
+                return _ore;
+            }
+            private set
+            {
+                if (value > 0)//Prestazioni da min 1h
+                    _ore = value;
+                else
+                    throw new Exception("Ore non valide");
+            }
+        }
+        [JsonProperty]
+        public DateTime Data
+        {
+            get
+            {
+                return _data;
+            }
+            private set
+            {
+                if (value != null)
+                    _data = value;
+                else
+                    throw new Exception("Data non valida");
+            }
+        }
+        [JsonProperty]
+        public string Tipo
+        {
+            get
+            {
+                return _tipo;
+            }
+            private set
+            {
+                if (value != null)
+                    _tipo = value;
+                else
+                    throw new Exception("Tipo non valido");
+            }
+        }
+
+        public Prestazione(string id, Persona giver, Persona reciver, int ore, DateTime data, string tipo)
+        {
+            Id = id;
+            Giver = giver;
+            Reciver = reciver;
+            Ore = ore;
+            Data = data;
+            Tipo = tipo;
+        }
+        public Prestazione()
+        {
+
+        }
+
+
+        protected Prestazione(Prestazione other) : this(other.Id, other.Giver, other.Reciver, other.Ore, other.Data, other.Tipo)
+        {
+        }
+        public Prestazione Clone()
+        {
+            return new Prestazione(this);
+        }
+        public bool Equals(Prestazione p)
+        {
+            if (p == null) return false;
+
+            if (this == p) return true;
+
+            return (this.Id == p.Id);
+        }
+        public override string ToString()
+        {
+            return $"Prestazione:{Id} {Giver}; {Reciver}; {Ore}; {Data}; {Tipo}";
+        }
+    }
+}
