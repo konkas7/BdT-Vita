a42010405fcce4391a5265c531082209e19cc3ef
konkas7 <76922391+konkas7@users.noreply.github.com>
2023-09-15 10:11:13 +0200

    Colori aggiornati

--- BdT Vita/Form1.Designer.cs
+++ BdT Vita/Form1.Designer.cs
@@ -131,6 +131,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1282, 564);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.AddPersona);
